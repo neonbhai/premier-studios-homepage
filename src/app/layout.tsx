07cb80335d32c15c143f186b511209cc53f0a870
Someshwar Tripathi <someshwartripathi8@gmail.com>
2025-08-21 09:21:08 +0530

    Remove the other fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter, Bricolage_Grotesque } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 // import NavBar from "./components/NavBar";
 import { Footer } from './components/Footer';
@@ -7,11 +7,7 @@ import { Footer } from './components/Footer';
 const inter = Inter({
     variable: '--font-inter',
     subsets: ['latin'],
-});
-
-const bricolageGrotesque = Bricolage_Grotesque({
-    variable: '--font-bricolage-grotesque',
-    subsets: ['latin'],
+    weight: ['400', '500', '700'],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +22,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body
-                className={`${inter.variable} ${bricolageGrotesque.variable} antialiased`}
-            >
+            <body className={`${inter.variable} antialiased`}>
                 {children}
                 <Footer />
             </body>
