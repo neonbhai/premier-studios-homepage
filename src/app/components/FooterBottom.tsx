fd4b460c39b4d6910782f278d7c97e031dd34100
Someshwar Tripathi <someshwartripathi8@gmail.com>
2025-08-27 07:28:38 +0530

    Add legal license to footer bottom & fix text

--- src/app/components/FooterBottom.tsx
+++ src/app/components/FooterBottom.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 export const FooterBottom: React.FC = () => {
     return (
         <div className="mt-8 flex w-full flex-col items-end justify-between pb-3 lg:flex-row lg:gap-12">
-            <div className="text-right text-xs text-white/40 md:text-base">
+            <div className="pb-2 text-right text-xs text-white/40 md:text-base">
                 © 2025 Premier Studios. All Rights Reserved.
             </div>
             <div className="flex gap-4 md:gap-8">
@@ -14,7 +14,13 @@ export const FooterBottom: React.FC = () => {
                     href="/terms"
                     className="text-xs text-white transition-colors hover:text-white/80 md:text-base"
                 >
-                    Terms & Condition
+                    Terms & Conditions
+                </Link>
+                <Link
+                    href="#"
+                    className="text-xs text-white transition-colors hover:text-white/80 md:text-base"
+                >
+                    Legal License
                 </Link>
                 <Link
                     href="/privacy-policy"
