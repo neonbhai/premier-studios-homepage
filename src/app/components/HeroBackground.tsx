05582d8a41f54b963343ddde62ee15e338ce29df
Someshwar Tripathi <someshwartripathi8@gmail.com>
2025-08-28 05:25:00 +0530

    Prettier

--- src/app/components/HeroBackground.tsx
+++ src/app/components/HeroBackground.tsx
@@ -13,13 +13,13 @@ export default function HeroBackground({
                 src={backgroundImage}
                 alt="Hero background"
                 fill
-                className="object-cover object-center rounded-3xl"
+                className="rounded-3xl object-cover object-center"
             />
             {/* If black overlay to the image is needed: */}
             <div className="absolute inset-0 bg-black/40 md:bg-black/40 lg:bg-black/40"></div>
-          
+
             {/* Right angled triangle div using CSS borders */}
-            <div className="absolute bottom-0 left-0 rotate-90 w-0 h-0 border-l-[60px] md:border-l-[100px] border-l-transparent border-b-[60px] md:border-b-[100px] border-b-black"></div>
-         </div>
+            <div className="absolute bottom-0 left-0 h-0 w-0 rotate-90 border-b-[60px] border-l-[60px] border-b-black border-l-transparent md:border-b-[100px] md:border-l-[100px]"></div>
+        </div>
     );
 }
