f21b5135135c7300d415103170df9d3170e9173d
sudheer1614@gmail.com <sudheer1614@gmail.com>
2025-08-28 04:42:23 +0530

    Refactor HeroBackground component for improved structure and styling

--- src/app/components/HeroBackground.tsx
+++ src/app/components/HeroBackground.tsx
@@ -8,15 +8,18 @@ export default function HeroBackground({
     backgroundImage,
 }: HeroBackgroundProps) {
     return (
-        <div className="hero-image-container absolute inset-0 m-[17px]">
+        <div className="absolute inset-0 m-[17px]">
             <Image
                 src={backgroundImage}
                 alt="Hero background"
                 fill
-                className="object-cover object-center"
+                className="object-cover object-center rounded-3xl"
             />
             {/* If black overlay to the image is needed: */}
-            {/* <div className="absolute inset-0 bg-black/30 md:bg-black/30 lg:bg-black/30"></div> */}
-        </div>
+            <div className="absolute inset-0 bg-black/40 md:bg-black/40 lg:bg-black/40"></div>
+          
+            {/* Right angled triangle div using CSS borders */}
+            <div className="absolute bottom-0 left-0 rotate-90 w-0 h-0 border-l-[60px] md:border-l-[100px] border-l-transparent border-b-[60px] md:border-b-[100px] border-b-black"></div>
+         </div>
     );
 }
