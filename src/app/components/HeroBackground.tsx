7b41bb03ed1d262776eaa858a154a2f01122e014
sudheer1614@gmail.com <sudheer1614@gmail.com>
2025-08-28 20:13:37 +0530

    Refactor HeroBackground and HeroSection components for improved styling and structure

--- src/app/components/HeroBackground.tsx
+++ src/app/components/HeroBackground.tsx
@@ -16,22 +16,22 @@ export default function HeroBackground({
                 className="z-0 rounded-3xl object-cover object-center"
             />
             {/* If black overlay to the image is needed: */}
-            {/* <div className="absolute inset-0 z-[1] bg-black/40 md:bg-black/40 lg:bg-black/40"></div> */}
+            <div className="absolute inset-0 z-[1] bg-black/40 md:bg-black/40 lg:bg-black/40"></div>
 
             {/* Responsive opaque SVG wedge matching mask cut (desktop/tablet) */}
             <svg
-                className="pointer-events-none absolute bottom-0 left-0 z-[2] hidden h-full w-full md:block"
+                className="pointer-events-none absolute bottom-0 left-0 z-[2] hidden h-full w-full md:block top-1"
                 viewBox="0 0 500 200"
                 preserveAspectRatio="none"
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
             >
-                <path d="M0 200 H34 Q30 200 27 197 L0 170 Z" fill="#050505" />
+                <path d="M0 200 H34 Q30 200 27 197 L0 170 Z" fill="#050505" stroke='#000000' />
             </svg>
 
             {/* Responsive opaque SVG wedge matching mobile cut */}
             <svg
-                className="pointer-events-none absolute bottom-0 left-0 z-[2] h-full w-full md:hidden"
+                className="pointer-events-none absolute bottom-0 left-0 z-[2] h-full w-full md:hidden top-1"
                 viewBox="0 0 361 383"
                 preserveAspectRatio="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -40,6 +40,7 @@ export default function HeroBackground({
                 <path
                     d="M0 383 H34.0705 Q30 383 27 380 L0 348.9295 Z"
                     fill="#050505"
+                    stroke='#050505'
                 />
             </svg>
         </div>
